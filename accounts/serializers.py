a4d0798898beffe12e9729798804f03fa1bf31f1
Esther Idumu <Idumuesther3@gmail.com>
2026-08-31 15:49:26 +0100

    -serializer complete
    -password validator implemented

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,2 +1,17 @@
 from rest_framework import serializers
 from accounts.models import User
+from django.contrib.auth.password_validation import validate_password as validate
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+    def validate_password(self, password):
+        validate(password)
+        return password
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
